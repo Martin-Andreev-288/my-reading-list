20d6126897dddc673ae6b563cd6dfcc06368e960
Martin Andreev <martin_i_a@abv.bg>
2025-03-03 17:03:51 +0200

    Implement the signup page and the form input components (for now with static data)
    Install the necessary shadcn components.
    Install the radix-ui/react-icons library.

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,5 @@
 export { default as Navbar } from "./layout/Navbar";
 export { default as BookForm } from "./book/BookForm";
 export { default as BookList } from "./book/BookList";
+export { default as FormInput } from "./formInput/FormInput";
+export { default as SubmitBtn } from "./submitBtn/SubmitBtn";
--- src/pages/signUp/SignUp.tsx
+++ src/pages/signUp/SignUp.tsx
@@ -1,5 +1,33 @@
-function SignUp() {
-  return <div>SignUp</div>;
+import { Form, Link } from "react-router";
+import { Card, CardHeader, CardContent, CardTitle } from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+import { SubmitBtn, FormInput } from "@/components";
+
+function Signup() {
+  return (
+    <section className="h-screen grid place-items-center">
+      <Card className="w-96 bg-muted">
+        <CardHeader>
+          <CardTitle className="text-center">Signup</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <Form method="post">
+            <FormInput type="text" name="username" />
+            <FormInput type="email" name="email" />
+            <FormInput type="password" name="password" />
+            <SubmitBtn text="Register" className="w-full mt-4" />
+
+            <p className="text-center mt-4">
+              Already a member?
+              <Button type="button" asChild variant="link">
+                <Link to="/login">Login</Link>
+              </Button>
+            </p>
+          </Form>
+        </CardContent>
+      </Card>
+    </section>
+  );
 }
 
-export default SignUp;
+export default Signup;
