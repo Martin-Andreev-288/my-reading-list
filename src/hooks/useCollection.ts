65fe0ceb3c91adfd289663376a8bc15203562f72
Martin Andreev <martin_i_a@abv.bg>
2025-03-04 17:25:00 +0200

    Add functionality for rendering books to the corresponding user's ID
    Now the user could see only the books added by himself (only the books which have the same ID as the corresponding user are rendered in his account).

--- src/hooks/useCollection.ts
+++ src/hooks/useCollection.ts
@@ -1,13 +1,21 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import { db } from "@/firebase/config";
-import { collection, onSnapshot } from "firebase/firestore";
+import { collection, onSnapshot, query, where, type Query, type DocumentData, type WhereFilterOp } from "firebase/firestore";
 import { type Book } from "@/utils/types";
 
-export const useCollection = (c: string) => {
+export const useCollection = (c: string, _q: [string, WhereFilterOp, string]) => {
     const [documents, setDocuments] = useState<Book[] | null>(null);
 
+    const q = useRef(_q).current;
+
     useEffect(() => {
-        const ref = collection(db, "books");
+        console.log('Testing for endless loop');
+
+        let ref: Query<DocumentData> = collection(db, c);
+
+        if (q) {
+            ref = query(ref, where(...q));
+        }
 
         const unsub = onSnapshot(ref, (snapshot) => {
             const results: Book[] = [];
@@ -19,7 +27,7 @@ export const useCollection = (c: string) => {
         });
 
         return () => unsub();
-    }, [c]);
+    }, [c, q]);
 
     return { documents }
 };
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,8 +1,15 @@
 import { Navbar, BookList, BookForm } from "@/components";
 import { useCollection } from "@/hooks/useCollection";
+import { useAuthContext } from "@/hooks/useAuthContext";
 
 export default function Home() {
-  const { documents: books } = useCollection("books");
+  const { user } = useAuthContext();
+
+  const { documents: books } = useCollection("books", [
+    "uid",
+    "==",
+    user?.uid as string,
+  ]);
 
   return (
     <div className="max-w-screen-sm mx-auto my-0">
