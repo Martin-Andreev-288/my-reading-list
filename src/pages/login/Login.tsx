9dd4c462a00645789e93f2aabacf0bd7b1af269f
Martin Andreev <martin_i_a@abv.bg>
2025-03-06 09:43:59 +0200

    Change the name of the FormInput name props for the email from "identifier" to "email"

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -25,7 +25,7 @@ function Login() {
           <form onSubmit={handleSubmit}>
             <FormInput
               type="email"
-              name="identifier"
+              name="email"
               value={email}
               onChange={(e) => setEmail(e.target.value)}
               required
